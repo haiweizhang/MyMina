f95d92c5932671b462ddcc3d061c3ccf397acea5
zhanghaiwei <deanzhang@163.com>
2013-10-21 19:46:31 +0800

    mina time server

--- src/MinaTimeServer.java
+++ src/MinaTimeServer.java
@@ -1,12 +1,21 @@
 
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
 
 public class MinaTimeServer 
 {
 	
 	public static void main(String[] args)
 	{
-		
+		IoAcceptor acceptor = new NioSocketAcceptor();
+		acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+		acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
 	}
 
 }
